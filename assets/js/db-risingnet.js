d2afd6ee07b6f5d7c3d775c8c20d3d517248eac2
daqbang-dot <daqbang@gmail.com>
2026-04-15 17:20:35 +0800

    Create assets/js/db-risingnet.js

---
+++ assets/js/db-risingnet.js
@@ -0,0 +1,10 @@
+// Fail: assets/js/db-risingnet.js
+// Pangkalan Data Berpusat: Harga Kos Pembekal
+
+const dbItems = [
+    // Data sebenar akan dimasukkan di sini selepas anda menampal teks
+    
+    // Contoh format yang akan saya jana:
+    // { id: "DS-2CE16D0T", category: "cctv", name: "2MP 1080p Bullet Camera", cost: 65.00 },
+    // { id: "DS-7204HQHI", category: "cctv", name: "4CH Turbo HD DVR", cost: 185.00 }
+];
